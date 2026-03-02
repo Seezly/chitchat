a9a3241ecc2753474753b05f2eb09a51d2d61b11
Sergio Gutierrez <sergiogutierrez0611@gmail.com>
2026-03-02 19:58:18 -0400

    feat: Rate limit added for sending messages and friend requests

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/conversation/{conversation}', [ConversationController::class, 'show
 Route::get('/conversation/with/{contact}', [ConversationController::class, 'conversationWith'])->name('conversation.withContact')->middleware('auth');
 Route::delete('/conversation/{conversation}', [ConversationController::class, 'destroyConversation'])->name('conversation.destroy')->middleware('auth');
 
-Route::post('/message/{conversation}', [MessageController::class, 'store'])->name('message.store')->middleware('auth');
+Route::post('/message/{conversation}', [MessageController::class, 'store'])->name('message.store')->middleware('auth', 'throttle:message');
 Route::delete('/message/{conversation}', [MessageController::class, 'destroyMessages'])->name('message.destroy-messages')->middleware('auth');
 
 Route::get('/profile', function () {
@@ -28,7 +28,7 @@ Route::get('/profile', function () {
 Route::put('/profile', [ProfileController::class, 'edit'])->name('profile.edit')->middleware('auth');
 
 Route::get('/contacts', ContactsController::class)->middleware('auth');
-Route::post('/contacts', [ContactsController::class, 'store'])->middleware('auth');
+Route::post('/contacts', [ContactsController::class, 'store'])->middleware('auth', 'throttle:friendRequests');
 Route::put('/contacts', [ContactsController::class, 'update'])->name('contact.update')->middleware('auth');
 
 Route::get('/contacts/search', [ContactsController::class, 'search'])->name('contact.search')->middleware('auth');
